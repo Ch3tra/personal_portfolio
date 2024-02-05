183162af3b50a7825ed2aef0c1f6ae5c6be79dc7
Ch3tra <Chetra.ou@yahoo.com>
2024-02-06 04:25:23 +0700

    update
    add dark-mode

--- script.js
+++ script.js
@@ -4,3 +4,45 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+let isDarkMode = false;
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-screen");
+    isDarkMode = !isDarkMode;
+}
+
+function updateToggleUI() {
+    if (isDarkMode) {
+        btntext.innerHTML = "Light";
+        btnicon.className = "fas fa-sun";
+        btntext_b.innerHTML = "Light";
+        btnicon_b.className = "fas fa-sun";
+    } else {
+        btntext.innerHTML = "Dark";
+        btnicon.className = "fas fa-moon";
+        btntext_b.innerHTML = "Dark";
+        btnicon_b.className = "fas fa-moon";
+    }
+}
+
+let desktopDarkbtn = document.getElementById("desktopDarkbtn");
+let mobileDarkbtn = document.getElementById("mobileDarkbtn");
+
+let btntext = document.getElementById("btntext");
+let btnicon = document.getElementById("btnicon");
+let btntext_b = document.getElementById("btntext-b");
+let btnicon_b = document.getElementById("btnicon-b");
+
+desktopDarkbtn.onclick = function () {
+    toggleDarkMode();
+    updateToggleUI();
+};
+
+mobileDarkbtn.onclick = function () {
+    toggleDarkMode();
+    updateToggleUI();
+};
+
+// Call updateToggleUI to set state to btn to synchronized between the desktop and mobile views
+updateToggleUI();
